538d9268d1202638ee1d0e06b656f061ec85b989
Deepanshu Mehra <deepanshumehra2102@gmail.com>
2024-10-18 23:55:20 +0530

    feat : created routes for auth

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,8 +1,17 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import { signinRouter } from './routes/signin';
+import { signupRouter } from './routes/signup';
+import { signoutRouter } from './routes/signout';
+import { currentUserRouter } from './routes/current-user';
 
 const app = express();
 app.use(bodyParser.json());
+
+app.use(signinRouter);
+app.use(signupRouter);
+app.use(signoutRouter);
+app.use(currentUserRouter);
 
 app.listen(3000, () => {
    console.log('Server running on port 3000!!!');
