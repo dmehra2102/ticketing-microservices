db9737db87f2eef30258becdfea9e0bcc681f1f2
Deepanshu Mehra <deepanshumehra2102@gmail.com>
2024-10-19 12:39:57 +0530

    feat :added error handling middleware

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,6 +4,7 @@ import { signinRouter } from './routes/signin';
 import { signupRouter } from './routes/signup';
 import { signoutRouter } from './routes/signout';
 import { currentUserRouter } from './routes/current-user';
+import { errorHandler } from './middlewares/error-handler.middleware';
 
 const app = express();
 app.use(bodyParser.json());
@@ -13,6 +14,10 @@ app.use(signupRouter);
 app.use(signoutRouter);
 app.use(currentUserRouter);
 
+app.use((err: Error, req: express.Request, res: express.Response, next: express.NextFunction) => {
+  errorHandler(err, req, res, next);
+});
+
 app.listen(3000, () => {
-   console.log('Server running on port 3000!!!');
+   console.log('Server running on port 3000');
 });
