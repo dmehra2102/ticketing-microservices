2a87b4916ff180f387fc22dfde97b00b250baacf
Deepanshu Mehra <deepanshumehra2102@gmail.com>
2024-10-24 00:07:06 +0530

    feat : added test cases for create ticket route and get ticket route

--- tickets/src/routes/createTicket.ts
+++ tickets/src/routes/createTicket.ts
@@ -5,6 +5,7 @@ import {
    requireAuthMiddleware,
    validateRequestMiddleware,
 } from '@dmehra2102-microservices-/common';
+import { Ticket } from '../models/ticket';
 
 const router = express.Router();
 
@@ -19,7 +20,18 @@ router.post(
          .withMessage('Price must be greater than 0.'),
    ],
    validateRequestMiddleware,
-   async (req: Request, res: Response) => {}
+   async (req: Request, res: Response) => {
+      const { title, price } = req.body;
+
+      const ticket = Ticket.build({
+         title,
+         price,
+         userId: req.currentUser!.id,
+      });
+      await ticket.save();
+
+      res.status(201).send(ticket);
+   }
 );
 
 export { router as createTicketRouter };
