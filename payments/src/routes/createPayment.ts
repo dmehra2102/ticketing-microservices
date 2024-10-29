cde06d47bac3461487ac60f858a9c88d01d083e4
Deepanshu Mehra <deepanshumehra2102@gmail.com>
2024-10-29 12:25:49 +0530

    feat : added stripe env in k8s secret

--- payments/src/routes/createPayment.ts
+++ payments/src/routes/createPayment.ts
@@ -1,9 +1,15 @@
 import {
+   BadRequestError,
    currentUserMiddleware,
+   NotAuthorizedError,
+   NotFoundError,
+   OrderStatus,
    requireAuthMiddleware,
+   validateRequestMiddleware,
 } from '@dmehra2102-microservices-/common';
-import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
+import express, { Request, Response } from 'express';
+import { Order } from '../models/order.model';
 
 const router = express.Router();
 
@@ -12,7 +18,17 @@ router.post(
    currentUserMiddleware,
    requireAuthMiddleware,
    [body('token').notEmpty(), body('orderId').notEmpty()],
-   async (req: Request, res: Response) => {}
+   validateRequestMiddleware,
+   async (req: Request, res: Response) => {
+      const { token, orderId } = req.body;
+      const order = await Order.findOne({ _id: orderId });
+
+      if (!order) throw new NotFoundError('Order not found.');
+      if (order.userId !== req.currentUser?.id) throw new NotAuthorizedError();
+      if (order.status === OrderStatus.CANCELLED) {
+         throw new BadRequestError('Cannot pay for an cancelled order');
+      }
+   }
 );
 
 export { router as createPaymentRoute };
